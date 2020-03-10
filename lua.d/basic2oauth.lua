279c4270ee81071be594eeae9b645db83304ddfe
gautsing <gautsing@cisco.com>
2020-03-10 14:41:47 -0700

    lua password parsing

--- lua.d/basic2oauth.lua
+++ lua.d/basic2oauth.lua
@@ -1,5 +1,35 @@
+
 require 'apache2'
 req = require('requests')
+
+-- Just for encoding --
+function char_to_hex (c)
+    return string.format("%%%02X", string.byte(c))
+end
+
+function post_urlencode(data)
+    if data == nil then
+        return
+    end
+    data = data:gsub("\n", "\r\n")
+    data = data:gsub("([^%w_%-%.~ ])", char_to_hex)
+    data = data:gsub(" ", "+")
+    return data
+end
+
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+end
+
 
 -- Just do nothing...
 function bypass(r)
@@ -20,6 +50,31 @@ function isempty(s)
         return false
     end
     return true
+end
+
+function is_header_basic(s)
+    if s:find("Basic ") then
+        return true
+    end
+    if s:find("basic ") then
+        return true
+    end
+    return false
+end
+
+function get_authorization_header_type(s)
+    local splitted = string.gmatch(s, '([^ ]+)')
+    return splitted()
+end
+
+function get_user_from_basic(s)
+    local splitted = string.gmatch(s, '([^:]+)')
+    return splitted()
+end
+
+function get_pass_from_basic(s)
+    local splitted = string.gmatch(s, '[^:]+:(.+)')
+    return splitted()
 end
 
 -- fetch envvar variables by reading the envvars.eifweb file
@@ -65,96 +120,122 @@ function envvar(r)
     return envt
 end
 
-
-
 -- intercepts a request which has Authorization header
 -- replaces the 'Basic <base64-user:pass>' with 'Bearer <access_token>'
 function basic2oauth(r)
     -- look for auth info
-    auth = r.headers_in['Authorization']
-    if auth ~= nil then
-        local auth_table = {}
-        local cred_table = {}
-        local auth_decoded = nil
-        local envt = envvar(r)
-        if isempty(envt['sso_url']) or isempty(envt['header']) then
-            r:err("basic2oauth (pass-thru) envvar is empty, init failed")
-            return apache2.OK
-        end
-        local sso_url = envt['sso_url']
-        local auth_header = envt['header']
-        for word in auth:gmatch("%w+") do
-            table.insert(auth_table, word)
-        end
-        if auth_table[1] ~= nil then
-            if auth_table[1]:lower() == 'basic' then
-                if auth_table[2] ~= nil then
-                    local auth_decoded = r:base64_decode(auth_table[2])
-                    for word in auth_decoded:gmatch("[^:]+") do
-                        table.insert(cred_table, word)
-                    end
-                    if #cred_table == 2 then
-                        local req_params = {
-                            grant_type = 'password',
-                            username = cred_table[1],
-                            password = r:escape(cred_table[2])
-                        }
-                        local req_headers = {
-                            ["Content-Type"] = "application/x-www-form-urlencoded",
-                            ["Authorization"] = auth_header,
-                            ["Cache-Control"] = "no-cache"
-                        }
-                        r:trace1(string.format("basic2oauth posting credentials for username: %s to url: %s", req_params['username'], sso_url))
-                        local res = req.post{
-                            sso_url,
-                            params = req_params,
-                            headers = req_headers,
-                            timeout = 15,
-                        }
+    local auth = r.headers_in['Authorization']
+    local enable_basic = r.headers_in['X-EIF-BASICAUTH-ENABLE']
+    local disable_oauth = r.headers_in['X-EIF-OAUTH-DISABLE']
+    local processed_basic2oauth = r.headers_in['X-EIF-BASICAUTH']
 
-                        if cred_table[1] == 'gautsing.test' then
-                            r:trace1(string.format("basic2oauth sleeping for 100 seconds"))
-                            os.execute("sleep " .. tonumber(100))
-                        end
+    if notempty(enable_basic) then
+        return apache2.OK
+    end
 
-                        if not res then
-                            r:err("basic2oauth (pass-thru) unable to fetch access token for username: %s", cred_table[1])
-                            return apache2.OK
-                        else
-                            if res.status_code == 200 then
-                                local tab = nil
-                                local err = nil
-                                tab, err = res.json()
-                                if tab ~= nil then
-                                    if tab.access_token ~= nil then
-                                        r.headers_in['Authorization'] = string.format("Bearer %s", tab.access_token)
-                                        r.headers_in['X-Basic-2-OAuth'] = string.format("%d", os.time())
-                                        r:trace1(string.format("basic2oauth (sucess) insert header for %s@%s", cred_table[1], tab.access_token))
-                                        r:err(   string.format("basic2oauth (sucess) insert header for %s", cred_table[1]))
-                                        return apache2.OK
-                                    else
-                                        r:debug(string.format("basic2oauth (pass-thru) no access_token found in: %s", tab))
-                                        return apache2.OK
-                                    end
-                                else
-                                    r:debug(string.format("basic2oauth (pass-thru) %s returned error %s", sso_url, err))
+    if notempty(disable_oauth) then
+        return apache2.OK
+    end
+
+    if notempty(processed_basic2oauth) then
+        return apache2.OK
+    end
+
+    if notempty(auth) then
+
+        if is_header_basic(auth) then
+            local envt = envvar(r)
+
+            if isempty(envt['sso_url']) or isempty(envt['header']) then
+                -- needs to be retried
+                r:err("basic2oauth (failure) passing header because: envvar is empty, init failed")
+                return apache2.OK
+            end
+
+            local sso_url = envt['sso_url']
+            local auth_header = envt['header']
+
+            local auth_encoded = string.sub(auth, 7)
+            if notempty(auth_encoded) then
+
+                local auth_decoded = r:base64_decode(auth_encoded)
+                local cred_user = get_user_from_basic(auth_decoded)
+                local cred_pass = get_pass_from_basic(auth_decoded)
+
+                if notempty(cred_user) and notempty(cred_pass) then
+                    local req_params = {
+                        grant_type = 'password',
+                        username = post_urlencode(cred_user),
+                        password = post_urlencode(cred_pass)
+                    }
+                    local req_headers = {
+                        ["Content-Type"] = "application/x-www-form-urlencoded",
+                        ["Authorization"] = auth_header,
+                        ["Cache-Control"] = "no-cache"
+                    }
+                    r:trace1(string.format("basic2oauth (debug) posting credentials to url: %s", sso_url))
+                    r:trace1(string.format("basic2oauth (debug) posting credentials for user: %s", req_params['username']))
+                    -- r:trace1(string.format("basic2oauth (debug) posting credentials for pw1: %s", req_params['password']))
+
+                    local res = req.post{
+                        sso_url,
+                        params = req_params,
+                        headers = req_headers,
+                        timeout = 15,
+                    }
+
+                    if not res then
+                        r.headers_in['X-EIF-BASICAUTH'] = string.format("failed 600")
+                        r:err("basic2oauth (failure) passing header because: api error when fetching access token for username: %s", cred_user)
+                        return apache2.OK
+                    else
+                        -- check status code
+                        if res.status_code == 200 then
+                            local tab = nil
+                            local err = nil
+                            tab, err = res.json()
+                            if tab ~= nil then
+                                if tab.access_token ~= nil then
+                                    r.headers_in['Authorization'] = string.format("Bearer %s", tab.access_token)
+                                    r.headers_in['X-EIF-BASICAUTH'] = string.format("success")
+                                    r:trace1(string.format("basic2oauth (debug) insert header for %s with token %s", cred_user, tab.access_token))
+                                    r:err(  string.format("basic2oauth (success) insert header for %s", cred_user))
                                     return apache2.OK
                                 end
-                           else
-                                r:debug(string.format("basic2oauth (pass-thru) %s returned status %s", sso_url, res.status_code))
-                                return apache2.OK
                             end
+                        else
+                            r:trace1(string.format("basic2oauth (debug) dumping res from url: %s %s", cred_pass, dump(res)))
+                            r:trace1(string.format("basic2oauth (debug) dumping res.json() from url: %s %s", cred_pass, dump(res.json())))
                         end
-                    else
-                        r:debug("basic2oauth (pass-thru) invalid authorization header")
+                        -- For all other cases it is a fail
+                        r.headers_in['X-EIF-BASICAUTH'] = string.format("failed %s ", res.status_code)
+                        r:err(string.format("basic2oauth (failure) passing header because: %s returned status %s", sso_url, res.status_code))
                         return apache2.OK
                     end
                 end
-            else
-                r:debug(string.format("basic2oauth (pass-thru) skipping '%s' type Authorization header", auth_table[1]:lower()))
+                -- For all other cases it is a fail
+                r.headers_in['X-EIF-BASICAUTH'] = string.format("failed 601")
+                r:err("basic2oauth (failure) passing header because: empty username or password")
                 return apache2.OK
             end
+            -- For all basic auth it is a fail
+            r.headers_in['X-EIF-BASICAUTH'] = string.format("failed 602")
+            r:err("basic2oauth (failure) passing header because: invalid basic auth header")
+            return apache2.OK
+        else
+            local auth_header_type = get_authorization_header_type(auth)
+            r:err(string.format("basic2oauth (bypass) passing header because: auth header type is %s", auth_header_type))
+            return apache2.OK
         end
     end
+    -- r.headers_in['X-EIF-BASICAUTH'] = string.format("bypass 000")
+    return apache2.OK
+end
+
+
+-- disable basic2oauth function
+function basic2oauth_disable(r)
+    -- r.headers_in['X-EIF-BASICAUTH'] = string.format("disable 000")
+    r:err(string.format("basic2oauth (disable) insert header for vh: %s", r.hostname))
     return apache2.OK
 end
